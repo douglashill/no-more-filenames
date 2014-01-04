4be3bce696a6f77fd40cb92783f9ea5e65b2c54d
Douglas Hill <hello@douglashill.co>
2014-01-04 21:59:57 +0000

    Find files to rename from arguments. Rename multiple files. Roughly cope with name conflicts.

--- File name from first line of note.py
+++ File name from first line of note.py
@@ -3,26 +3,41 @@
 
 # http://stackoverflow.com/questions/295135/turn-a-string-into-a-valid-filename-in-python
 
-import os, os.path, re
+import sys, os, os.path, re
 
 max_length = 100
 
-file_name = 'Untitled.txt'
+def getTitle(file_path):
+	with open(file_path) as text_file:
+		first_line = text_file.readline()
+		
+		# Blacklist: [ ] / \ = + < > : ; " , * .
+		# https://forums.dropbox.com/topic.php?id=23023
+		title = re.sub('[\[\]/\\=\+<>:;",\*\.]+', '', first_line)
+		title = title.strip('#') # Markdown title marks
+		if len(title) > max_length:
+			title = title[:max_length]
+		title = re.sub('\s{2,}', ' ', title)
+		title = title.strip()
+		
+		return title
 
-with open(file_name) as text_file:
-	first_line = text_file.readline()
+def refreshFileName(file_path):
+	title = getTitle(file_path)
 	
-	# Blacklist: [ ] / \ = + < > : ; " , * .
-	# https://forums.dropbox.com/topic.php?id=23023
-	title = re.sub('[\[\]/\\=\+<>:;",\*\.]+', '', first_line)
-	title = title.strip('#') # Markdown title marks
-	if len(title) > max_length:
-		title = title[:max_length]
-	title = re.sub('\s{2,}', ' ', title)
-	title = title.strip()
-	print('The title is: “' + title + '”.')
+	file_name = os.path.basename(file_path)
+	old_base_name = os.path.splitext(file_name)[0]
+	extension = os.path.splitext(file_name)[1]
+	
+	if old_base_name == title:
+		return
+	
+	counter = 1
+	while os.path.exists(os.path.join(os.path.dirname(file_path), title + extension)):
+		++counter
+		title = title + ' ' + str(counter)
+	
+	os.rename(file_path, os.path.join(os.path.dirname(file_path), title + extension))
 
-extension = os.path.splitext(file_name)[1]
-new_file_name = title + extension
-print('The new file name is ' + new_file_name)
-os.rename(file_name, new_file_name)
+for path in sys.argv[1:]:
+	refreshFileName(path)
